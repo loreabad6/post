4a04e575e1a9feb77abb70dc79db83988bda212c
loreabad6 <lore.abad6@gmail.com>
2024-08-01 21:22:39 +0200

    feat: add datetime column to artificial data :gift:

--- R/data.R
+++ R/data.R
@@ -1,14 +1,15 @@
-#' Random polygons to exemplify {post} functions
+#' Random polygons to exemplify post functions
 #'
-#' Five random polygons with 5 timestamp states are artificially
-#' generated to exemplify the main funcitonality of post.
-#' See [data-raw/polygons.R] for code on its creation.
+#' Five random polygons with 5 time-stamp states are artificially
+#' generated to exemplify the main functionality of post.
+#' See `data-raw/polygons.R` for code on its creation.
 #'
 #' @format An object of class [sf] with `POLYGON` geometries,
+#' and `Date`
 #' with 25 rows and 3 columns:
 #' \describe{
 #'  \item{gid}{ID of polygon geometry}
-#'  \item{tid}{ID of polygon timestamp}
+#'  \item{datetime}{the datetime of the polygon geometry}
 #'  \item{geometry}{the geometry list column}
 #' }
 "polygons"
--- data-raw/polygons.R
+++ data-raw/polygons.R
@@ -31,17 +31,25 @@ pol_t0 = st_voronoi(pts_t0, envelope = bb) |>
 pol_t1 = st_jitter(pol_t0, factor = 0.05)
 pol_t2 = st_jitter(pol_t1, factor = 0.05) |>
   st_simplify(dTolerance = 0.1)
-pol_t3 = st_jitter(pol_t2, factor = 0.02)
+pol_t3 = st_jitter(pol_t2, factor = 0.02)  |>
+  st_buffer(-0.02)
 pol_t4 = st_jitter(pol_t0, factor = 0.07)
+
+# Set dates
+date = seq(
+  from = as.Date("2020-10-01"),
+  to = as.Date("2020-10-05"),
+  length.out = 5
+)
 
 # Combine results in one sf object and add CRS
 polygons = rbind(
-  st_sf(gid = letters[1:5], tid = 1, geometry = pol_t0),
-  st_sf(gid = letters[1:5], tid = 2, geometry = pol_t1),
-  st_sf(gid = letters[1:5], tid = 3, geometry = pol_t2),
-  st_sf(gid = letters[1:5], tid = 4, geometry = pol_t3),
-  st_sf(gid = letters[1:5], tid = 5, geometry = pol_t4)
+  st_sf(gid = letters[1:5], datetime = date[[1]], geometry = pol_t0),
+  st_sf(gid = letters[1:5], datetime = date[[2]], geometry = pol_t1),
+  st_sf(gid = letters[1:5], datetime = date[[3]], geometry = pol_t2),
+  st_sf(gid = letters[1:5], datetime = date[[4]], geometry = pol_t3),
+  st_sf(gid = letters[1:5], datetime = date[[5]], geometry = pol_t4)
 ) |> st_set_crs(4326)
 
 # Save as data for the package
-usethis::use_data(polygons)
+usethis::use_data(polygons, overwrite = TRUE)
