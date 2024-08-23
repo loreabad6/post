692c9c089223c1e50fc4ae390dd01067866946a1
loreabad6 <lore.abad6@gmail.com>
2024-08-23 15:13:51 +0200

    feat: add face_spatial and face_temporal methods for post_table :gift:

--- R/cubble.R
+++ R/cubble.R
@@ -1,20 +1,42 @@
-#' Face temporal method for post_table
+#' Face spatial/temporal method for post_table
+#'
+#' `face_spatial` pivots a `post_table` object from a long (temporal)
+#' to a nested (spatial) form.
 #'
 #' `face_temporal()` pivots a `post_table` object from a nested (spatial)
-#' to a long (temporal) form. 
-#' The method for `post_table` objects activates the changing geometry 
+#' to a long (temporal) form.
+#' For `post_table` objects, it activates the changing geometry
 #' in the long form.
 #'
 #' @param data a post_table object
-#' @param col see the documentation of cubble::face_temporal for details.
+#' @param col see `cubble::face_temporal()` for details.
 #'
-#' @name cubble
+#' @name cubble-face
 #' @return a post_table object
 #'
-#' importFrom cubble face_temporal
+#' @importFrom cubble face_temporal
+#' @importFrom sf st_set_geometry
 #' @export
 face_temporal.post_table = function(data, col) {
-	out = cubble::face_temporal(data, col)
-	st_geometry(out) = attr(data, "sf_column_post")
-	out
+	structure(
+	  sf::st_set_geometry(
+	    NextMethod(),
+	    attr(data, "sf_column_post")
+	  ),
+	  class = union(c("post_table", "sf"), class(NextMethod())),
+	  sf_column_post = attr(data, "sf_column_post"),
+	  time_column = attr(data, "time_column"),
+	  geom_sum_fun = attr(data, "geom_sum_fun")
+	)
+}
+
+#' @rdname cubble-face
+#'
+#' @importFrom cubble face_spatial
+#' @export
+face_spatial.post_table = function(data, col) {
+  structure(
+    NextMethod(),
+    class = union(c("post_table"), class(NextMethod()))
+  )
 }
