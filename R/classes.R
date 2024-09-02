2b4036c35de14faa10577d2d295e51f988c6fb65
loreabad6 <lore.abad6@gmail.com>
2024-09-02 15:15:21 +0200

    feat: add restore and remove functions for post_array :gift:

--- R/classes.R
+++ R/classes.R
@@ -1,8 +1,9 @@
 #' @description
-#' Utility functions to remove/restore post_table classes when passing
-#' functions onto spatial_cubble_df and temporal_cubble_df
+#' Utility functions to remove/restore post_table/post_array classes when
+#' passing functions onto stars, spatial_cubble_df and temporal_cubble_df
 #'
-#' @return An object that inherits or removes the classes post_table and sf.
+#' @return An object that inherits or removes the classes post_table/post_array
+#' and sf.
 #'
 #' @param x An object that inherits post_table class.
 #' @noRd
@@ -44,5 +45,32 @@ restore_temporal_post_table = function(x) {
       cubble_temp_classes,
       setdiff(class(x), cubble_temp_classes)
     )
+  )
+}
+#' @param x An object that inherits stars class.
+#' @noRd
+remove_post_array = function(x) {
+  class(x) = setdiff(class(x), "post_array")
+  x
+}
+#' @param x An object that inherits stars class.
+#' @noRd
+restore_post_array = function(x, x_orig) {
+  # Extract sf_column name, default to first sfc attribute in stars object
+  sf_column = names(lapply(x, \(i) inherits(i, "sfc")))[[1]]
+  # Identify spatial dimension
+  sf_dim = names(which(sapply(st_dimensions(x_orig), \(i) inherits(i$value, "sfc"))))[1]
+  # Extract to and from indices for spatial dimension
+  from = stars::st_dimensions(x)[[sf_dim]]$from
+  to = stars::st_dimensions(x)[[sf_dim]]$to
+
+  structure(
+    x,
+    class = union("post_array", class(x)),
+    group_id_colname = attr(x_orig, "group_id_colname"),
+    group_ids = attr(x_orig, "group_ids")[from:to],
+    sf_column = sf_column,
+    time_column = attr(x_orig, "time_column"),
+    agr =  sf::st_agr(x)
   )
 }
