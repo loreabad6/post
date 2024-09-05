5cf5170335e1a33451ca785530b1f8c64d2333c6
loreabad6 <lore.abad6@gmail.com>
2024-09-05 16:18:14 +0200

    test: add tests for stars functions :test_tube:

---
+++ tests/testthat/test-stars.R
@@ -0,0 +1,15 @@
+suppressPackageStartupMessages(library(sf))
+
+poly = st_point(c(0.5,0.5)) |>
+  st_sfc(crs = 4326) |>
+  st_buffer(10000)
+cls = "post_array"
+
+test_that("subsetting post_array works as expected", {
+  out1 = as_post_array(
+    polygons,
+    geometry_summary = summarise_geometry_union
+  )[poly]
+  expect_snapshot(out1)
+  expect_s3_class(out1, cls)
+})
