2f490867d7b2f99dadf9e6f452a7055330236d5d
loreabad6 <lore.abad6@gmail.com>
2024-09-06 13:43:07 +0200

    test: avoid snapshot dplyr tests for post_table :test_tube:

--- tests/testthat/test-dplyr.R
+++ tests/testthat/test-dplyr.R
@@ -8,11 +8,11 @@ tab = as_post_table(polygons)
 cls_tab = c("post_table", "sf")
 
 test_that("arrange works as expected and returns a post_table", {
-  out1 = arrange(tab, gid)
-  expect_snapshot(out1)
+  out1 = arrange(tab, lat)
+  expect_equal(out1$gid, c("e", "d", "c", "b", "a"))
   expect_s3_class(out1, cls_tab)
   out2 = arrange(face_temporal(tab), datetime)
-  expect_snapshot(out2)
+  expect_equal(out2$gid, rep(letters[1:5], times = 5))
   expect_s3_class(out2, cls_tab)
 })
 test_that("filter works as expected and returns a post_table", {
@@ -20,23 +20,23 @@ test_that("filter works as expected and returns a post_table", {
   expect_snapshot(out1)
   expect_s3_class(out1, cls_tab)
   out2 = filter(face_temporal(tab), gid %in% c("b", "c"))
-  expect_snapshot(out2)
+  expect_equal(out2$gid, rep(letters[2:3], each = 5))
   expect_s3_class(out2, cls_tab)
 })
 test_that("mutate works as expected and returns a post_table", {
   out1 = mutate(tab, foo = "bar")
-  expect_snapshot(out1)
+  expect_equal(names(out1), c("gid", "long", "lat", "geom_sum", "ts", "foo"))
   expect_s3_class(out1, cls_tab)
   out2 = mutate(face_temporal(tab), foo = "bar")
-  expect_snapshot(out2)
+  expect_equal(names(out2), c("gid", "datetime", "geometry", "foo"))
   expect_s3_class(out2, cls_tab)
 })
 test_that("rename works as expected and returns a post_table", {
   out1 = rename(tab, foo = gid)
-  expect_snapshot(out1)
+  expect_equal(names(out1), c("foo", "long", "lat", "geom_sum", "ts"))
   expect_s3_class(out1, cls_tab)
   out2 = rename(face_temporal(tab), foo = gid)
-  expect_snapshot(out2)
+  expect_equal(names(out2), c("foo", "datetime", "geometry"))
   expect_s3_class(out2, cls_tab)
 })
 test_that("rename updates sf_column", {
@@ -51,24 +51,24 @@ test_that("rename updates sf_column", {
 })
 test_that("select works as expected and returns a post_table", {
   expect_message(
-    out1 <- select(tab, lat),
-    "Missing attribute `gid`, `long`, and `ts`, add it back."
+    out1 <- select(mutate(tab, foo = "bar", apple = 2), foo),
+    "Missing attribute `gid`, `long`, `lat`, and `ts`, add it back."
   )
-  expect_snapshot(out1)
+  expect_equal(names(out1), c("gid", "long", "lat", "ts", "foo", "geom_sum"))
   expect_s3_class(out1, cls_tab)
   expect_message(
     out2 <- select(face_temporal(tab), datetime),
     "Missing attribute `gid`, add it back."
   )
-  expect_snapshot(out2)
+  expect_equal(names(out2), c("gid", "datetime", "geometry"))
   expect_s3_class(out2, cls_tab)
 })
 test_that("slice works as expected and returns a post_table", {
   out1 = slice(tab, 2:3)
-  expect_snapshot(out1)
+  expect_equal(out1$gid, c("b", "c"))
   expect_s3_class(out1, cls_tab)
   out2 = slice(face_temporal(tab), 2:3)
-  expect_snapshot(out2)
+  expect_equal(out2$gid, c("a", "a"))
   expect_s3_class(out2, cls_tab)
 })
 
