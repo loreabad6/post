ca28ceb971df3beaec26a8567f3ca7f913d2102f
loreabad6 <lore.abad6@gmail.com>
2024-09-06 13:55:39 +0200

    test: avoid snapshot stars tests :test_tube:

--- tests/testthat/test-stars.R
+++ tests/testthat/test-stars.R
@@ -11,6 +11,9 @@ test_that("subsetting post_array works as expected", {
     polygons,
     geometry_summary = summarise_geometry_union
   )
-  expect_snapshot(out1[poly])
+  expect_equal(length(out1[poly]$geometry), 10)
+  expect_equal(attr(out1[poly], "group_ids"), c("c", "d"))
+  expect_equal(st_dimensions(out1[poly])$geom_sum$from, 3)
+  expect_equal(st_dimensions(out1[poly])$geom_sum$to, 4)
   expect_s3_class(out1[poly], cls)
 })
