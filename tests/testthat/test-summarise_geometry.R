4c1fd95e01c1144e2c6810576bbd44c4690037c8
loreabad6 <lore.abad6@gmail.com>
2024-09-09 10:53:06 +0200

    test: warning for vector group_id :test_tube:

--- tests/testthat/test-summarise_geometry.R
+++ tests/testthat/test-summarise_geometry.R
@@ -2,4 +2,8 @@ test_that("summarise_geometry_* functions works as expected", {
   expect_snapshot(summarise_geometry_bbox(polygons))
   expect_snapshot(summarise_geometry_union(polygons))
   expect_snapshot(summarise_geometry_centroid(polygons))
+  expect_warning(
+    summarise_geometry_union(polygons, group_id = rep(1:5, times = 5)),
+    "assuming correct order and unique timestamps per group"
+  )
 })
