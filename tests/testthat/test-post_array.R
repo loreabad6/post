68dd0a7f1e73648f8062ce6139c9fec90356248d
loreabad6 <lore.abad6@gmail.com>
2025-02-21 15:19:21 +0100

    test: as_post_array.stars tests :test_tube:

--- tests/testthat/test-post_array.R
+++ tests/testthat/test-post_array.R
@@ -1,3 +1,6 @@
+suppressPackageStartupMessages(library(sf))
+suppressPackageStartupMessages(library(stars))
+
 # test_that("as_post_array works as expected", {
   # expect_snapshot(as_post_array(polygons))
   # expect_snapshot(as_post_array(as_post_table(polygons)))
@@ -12,6 +15,29 @@ test_that("as_post_table default to the expected values", {
                   sf_column_name = "geometry",
                   geometry_summary = summarise_geometry_centroid)
   )
+})
+
+pnt = st_sample(st_as_sfc(st_bbox(polygons)), 10)
+dates = c(Sys.Date()-1, Sys.Date())
+
+vdc1 = st_sf(rdm = rnorm(20), polygons = st_buffer(st_sample(st_bbox(pnt), 20), 500),
+             geometry = rep(pnt, 2), date = rep(dates, each = 10)) |>
+  st_as_stars(dims = c("geometry", "date"))
+
+vdc2 = st_sf(rdm = rnorm(20),
+             geometry = rep(pnt, 2), date = rep(dates, each = 10)) |>
+  st_as_stars(dims = c("geometry", "date"))
+
+test_that("as_post_array for stars keeps stars object with added attributes", {
+  expect_equal(as_post_array(vdc1), vdc1, ignore_attr = TRUE)
+  expect_s3_class(as_post_array(vdc1), "post_array")
+  expect_equal(attr(as_post_array(vdc1), "group_ids"), 1:10)
+  expect_equal(attr(as_post_array(vdc1), "group_id_colname"), "gid")
+  expect_equal(attr(as_post_array(vdc1), "sf_column"), "polygons")
+})
+
+test_that("as_post_array fails for stars without sfc columns", {
+  expect_error(as_post_array(vdc2), "`x` does not have a <sfc> attribute present")
 })
 
 # test_that("get_group_ids works as expected", {
